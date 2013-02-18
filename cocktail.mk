c29e2764fdfb07876479da584ad73539e89b2601
bagyusz <phelyx92@gmail.com>
2013-02-18 21:51:01 +0100

    battery fix, wifi tethering fix, add BT headset, add compass, support USB OTG, etc..
    Signed-off-by: bagyusz <phelyx92@gmail.com>

--- cocktail.mk
+++ cocktail.mk
@@ -8,8 +8,13 @@ $(call inherit-product-if-exists, device/alcatel/cocktail/vendor_cocktail.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/alcatel/cocktail/overlay
 
+# Qualcomm scripts
+PRODUCT_COPY_FILES += \
+    device/alcatel/cocktail/prebuilt/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh
+
 # init.rc and ueventd.rc
 PRODUCT_COPY_FILES += \
+    device/alcatel/cocktail/init.rc:root/init.rc \
     device/alcatel/cocktail/init.cocktail.rc:root/init.cocktail.rc \
     device/alcatel/cocktail/init.cocktail.usb.rc:root/init.cocktail.usb.rc \
     device/alcatel/cocktail/ueventd.cocktail.rc:root/ueventd.cocktail.rc
@@ -24,25 +29,36 @@ PRODUCT_COPY_FILES += \
     device/alcatel/cocktail/ft5306_ts.kcm:system/usr/keychars/ft5306_ts.kcm \
     device/alcatel/cocktail/ft5306_ts.kl:system/usr/keylayout/ft5306_ts.kl \
 
+# apn fix
+PRODUCT_COPY_FILES += \
+    device/alcatel/cocktail/prebuilt/apns-conf.xml:system/etc/apns-conf.xml \
+    device/alcatel/cocktail/prebuilt/spn-conf.xml:system/etc/spn-conf.xml
+
 # Media codecs & audio policy
 PRODUCT_COPY_FILES += \
     device/alcatel/cocktail/media_codecs.xml:system/etc/media_codecs.xml \
     device/alcatel/cocktail/media_profiles.xml:system/etc/media_profiles.xml \
     device/alcatel/cocktail/audio_policy.conf:system/etc/audio_policy.conf
 
+
+# CNE config
+PRODUCT_COPY_FILES += \
+   device/alcatel/cocktail/OperatorPolicy.xml:system/etc/OperatorPolicy.xml \
+   device/alcatel/cocktail/UserPolicy.xml:system/etc/UserPolicy.xml
+
 # Use HDPI phone dalvik config
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
-# HDPI resources
-PRODUCT_AAPT_CONFIG += hdpi
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
@@ -53,7 +69,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 # Kernel - prebuilt for now
 ifeq ($(TARGET_PREBUILT_KERNEL),)
@@ -65,28 +82,65 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+
 # Additional packages
 PRODUCT_PACKAGES += \
     copybit.msm7x30 \
     gralloc.msm7x30 \
     hwcomposer.msm7x30 \
-    audio.primary.msm7x30 \
-    audio_policy.msm7x30 \
     libgenlock \
+    libmemalloc \
+    liboverlay \
+    libQcomUI \
     libtilerenderer \
-    libinvensense_hal \
-    gps.cocktail
+    libinvensense_hal
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio_policy.msm7x30 \
+    audio.primary.msm7x30 \
+    libaudioutils
 
 # OMX
 PRODUCT_PACKAGES += \
-    libOmxCore \
-    libOmxVdec \
-    libOmxVenc \
-    libmm-omxcore \
-    libdivxdrmdecrypt
+    libdivxdrmdecrypt \
+    libI420colorconvert \
+    mm-vdec-omx-test \
+    mm-venc-omx-test720p \
+    mm-video-driver-test \
+    mm-video-encdrv-test \
+    libOmxCore.so \
+    libOmxVdec.so \
+    libOmxVenc.so \
+    libstagefrighthw
+
+# GPS
+PRODUCT_PACKAGES += \
+    librpc \
+    gps.cocktail
+
+# Misc
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+# Power
+PRODUCT_PACKAGES += \
+    power.msm7x30
+
+# HDMI
+PRODUCT_PACKAGES += \
+    hdmid
 
 # Ship Torch
 PRODUCT_PACKAGES += Torch
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    e2fsck \
+    make_ext4fs \
+    setup_fs
+
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240 \
@@ -95,15 +149,26 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # System properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/system/lib/libril-qc-qmi-1.so \
-    rild.libargs=-d/dev/smd0 \
-    ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10 \
-    ro.telephony.ril_class=QualcommSharedRIL \
-    ro.bt.bdaddr_path=/system/etc/bluetooth/bdaddr.txt \
-debug.sf.nobootanimation=1 \
-persist.sys.shutdown.mode=hibernate \
-    persist.sys.usb.config=mass_storage \
+    persist.cne.UseCne=vendor \
+    persist.cne.UseSwim=false \
+    persist.cne.bat.range.low.med=30 \
+    persist.cne.bat.range.med.high=60 \
+    persist.cne.loc.policy.op=/system/etc/OperatorPolicy.xml \
+    persist.cne.loc.policy.user=/system/etc/UserPolicy.xml \
+    persist.cne.bwbased.rat.sel=false \
+    persist.cne.snsr.based.rat.mgt=false \
+    persist.cne.bat.based.rat.mgt=false \
+    persist.cne.rat.acq.time.out=30000 \
+    persist.cne.rat.acq.retry.tout=0 \
+    persist.cne.nsrm.mode=false \
+    debug.sf.nobootanimation=1 \
+    persist.sys.shutdown.mode=hibernate \
+    persist.sys.usb.config=mass_storage
+
+
+# QCOM Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.hw=1 \
+    debug.egl.hw=1
 
 $(call inherit-product, build/target/product/full.mk)
-
--- vendor_cocktail.mk
+++ vendor_cocktail.mk
@@ -1,6 +1,7 @@
 # prebuilt libaudioalsa
 PRODUCT_COPY_FILES += \
-    device/alcatel/cocktail/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so
+    device/alcatel/cocktail/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so \
+    device/alcatel/cocktail/proprietary/libaudcal.so:obj/lib/libaudcal.so
 
 # bcm4330 wlan fw
 PRODUCT_COPY_FILES += \
@@ -9,12 +10,40 @@ PRODUCT_COPY_FILES += \
     device/alcatel/cocktail/proprietary/fw_bcmdhd_p2p.bin:/system/etc/firmware/fw_bcmdhd_p2p.bin \
     device/alcatel/cocktail/bcmdhd.cal:/system/etc/wifi/bcmdhd.cal
 
+
+# USB-OTG
+PRODUCT_COPY_FILES += \
+    device/alcatel/cocktail/proprietary/usbhub:/system/bin/usbhub \
+    device/alcatel/cocktail/proprietary/usbhub_init:/system/bin/usbhub_init
+
+# hdmi
+PRODUCT_COPY_FILES += \
+    device/alcatel/cocktail/proprietary/hdmid:/system/bin/hdmid
+
 # bcm4330 bt fw
 PRODUCT_COPY_FILES += \
     device/alcatel/cocktail/proprietary/bcm4330.hcd:/system/etc/firmware/bcm4330.hcd
 
+# battery
+PRODUCT_COPY_FILES += \
+    device/alcatel/cocktail/proprietary/battery_charging:/system/bin/battery_charging \
+    device/alcatel/cocktail/proprietary/battery_charge_0.rle:/system/battery_charge_0.rle \
+    device/alcatel/cocktail/proprietary/battery_charge_1.rle:/system/battery_charge_1.rle \
+    device/alcatel/cocktail/proprietary/battery_charge_2.rle:/system/battery_charge_2.rle \
+    device/alcatel/cocktail/proprietary/battery_charge_3.rle:/system/battery_charge_3.rle \
+    device/alcatel/cocktail/proprietary/battery_charge_4.rle:/system/battery_charge_4.rle \
+    device/alcatel/cocktail/proprietary/battery_charge_5.rle:/system/battery_charge_5.rle \
+    device/alcatel/cocktail/proprietary/battery_charge_6.rle:/system/battery_charge_6.rle \
+    device/alcatel/cocktail/proprietary/battery_charge_7.rle:/system/battery_charge_7.rle \
+    device/alcatel/cocktail/proprietary/battery_charge_8.rle:/system/battery_charge_8.rle \
+    device/alcatel/cocktail/proprietary/battery_charge_9.rle:/system/battery_charge_9.rle \
+    device/alcatel/cocktail/proprietary/battery_charge_full.rle:/system/battery_charge_full.rle \
+    device/alcatel/cocktail/proprietary/initlogo.rle:/system/initlogo.rle
+
 # video codecs
 PRODUCT_COPY_FILES += \
+    device/alcatel/cocktail/proprietary/firmware/leia_pfp_470.fw:/system/etc/firmware/leia_pfp_470.fw \
+    device/alcatel/cocktail/proprietary/firmware/leia_pm4_470.fw:/system/etc/firmware/leia_pm4_470.fw \
     device/alcatel/cocktail/proprietary/firmware/vidc_720p_command_control.fw:/system/etc/firmware/vidc_720p_command_control.fw \
     device/alcatel/cocktail/proprietary/firmware/vidc_720p_h263_dec_mc.fw:/system/etc/firmware/vidc_720p_h263_dec_mc.fw \
     device/alcatel/cocktail/proprietary/firmware/vidc_720p_h264_dec_mc.fw:/system/etc/firmware/vidc_720p_h264_dec_mc.fw \
@@ -23,9 +52,12 @@ PRODUCT_COPY_FILES += \
     device/alcatel/cocktail/proprietary/firmware/vidc_720p_mp4_enc_mc.fw:/system/etc/firmware/vidc_720p_mp4_enc_mc.fw \
     device/alcatel/cocktail/proprietary/firmware/vidc_720p_vc1_dec_mc.fw:/system/etc/firmware/vidc_720p_vc1_dec_mc.fw
 
+
+
 # sensors
 PRODUCT_COPY_FILES += \
-    device/alcatel/cocktail/proprietary/sensors.qcom.so:/system/lib/hw/sensors.cocktail.so
+    device/alcatel/cocktail/proprietary/sensors.qcom.so:/system/lib/hw/sensors.cocktail.so \
+    device/alcatel/cocktail/proprietary/akmd8975:/system/bin/akmd8975
 
 # adreno
 PRODUCT_COPY_FILES += \
@@ -37,15 +69,23 @@ PRODUCT_COPY_FILES += \
     device/alcatel/cocktail/proprietary/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
     device/alcatel/cocktail/proprietary/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
     device/alcatel/cocktail/proprietary/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
-    device/alcatel/cocktail/proprietary/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
-    device/alcatel/cocktail/proprietary/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
+    device/alcatel/cocktail/proprietary/firmware/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
+    device/alcatel/cocktail/proprietary/firmware/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
     device/alcatel/cocktail/proprietary/libsc-a2xx.so:/system/lib/libsc-a2xx.so
 
 # misc
 PRODUCT_COPY_FILES += \
     device/alcatel/cocktail/proprietary/rmt_storage:/system/bin/rmt_storage \
     device/alcatel/cocktail/proprietary/libaudioalsa.so:/system/lib/libaudioalsa.so \
-    device/alcatel/cocktail/proprietary/lights.msm7630_surf.so:/system/lib/hw/lights.cocktail.so \
+    device/alcatel/cocktail/proprietary/libaudcal.so:/system/lib/libaudcal.so \
+    device/alcatel/cocktail/proprietary/lights.msm7630_surf.so:/system/lib/hw/lights.cocktail.so
+
+#    device/alcatel/cocktail/proprietary/libmmosal.so:/system/lib/libmmosal.so \
+# Media libraries
+#PRODUCT_COPY_FILES += \
+#    device/alcatel/cocktail/proprietary/libmmparser.so:/system/lib/libmmparser.so \
+#    device/alcatel/cocktail/proprietary/libwvm.so:/system/lib/libwvm.so \
+#    device/alcatel/cocktail/proprietary/libWVStreamControlAPI_L3.so:/system/lib/libWVStreamControlAPI_L3.so
 
 # camera
 PRODUCT_COPY_FILES += \
@@ -54,11 +94,16 @@ PRODUCT_COPY_FILES += \
     device/alcatel/cocktail/proprietary/libgemini.so:/system/lib/libgemini.so \
     device/alcatel/cocktail/proprietary/libmmipl.so:/system/lib/libmmipl.so \
     device/alcatel/cocktail/proprietary/libmmjpeg.so:/system/lib/libmmjpeg.so \
+    device/alcatel/cocktail/proprietary/libmm-color-convertor.so:/system/lib/libmm-color-convertor.so \
     device/alcatel/cocktail/proprietary/libchromatix_ov5647_preview.so:/system/lib/libchromatix_ov5647_preview.so
+
+#    device/alcatel/cocktail/proprietary/libchromatix_imx074_default_video.so:/system/lib/libchromatix_imx074_default_video.so \
+#    device/alcatel/cocktail/proprietary/libchromatix_imx074_preview.so:/system/lib/libchromatix_imx074_preview.so \
+#    device/alcatel/cocktail/proprietary/libchromatix_imx074_zsl.so:/system/lib/libchromatix_imx074_zsl.so \
+#    device/alcatel/cocktail/proprietary/libchromatix_ov5647_default_video.so:/system/lib/libchromatix_ov5647_default_video.so \
 
 # ril
 PRODUCT_COPY_FILES += \
-    device/alcatel/cocktail/proprietary/libdiag.so:/system/lib/libdiag.so \
     device/alcatel/cocktail/proprietary/libidl.so:/system/lib/libidl.so \
     device/alcatel/cocktail/proprietary/libnetmgr.so:/system/lib/libnetmgr.so \
     device/alcatel/cocktail/proprietary/libqdp.so:/system/lib/libqdp.so \
